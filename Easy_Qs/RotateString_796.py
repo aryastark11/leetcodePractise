201e8acba69eb2faba9c2a4c7891dbda51082da1
aryastark11 <36256296+aryastark11@users.noreply.github.com>
2021-02-24 18:45:37 +0530

    Add files via upload

--- Easy_Qs/RotateString_796.py
+++ Easy_Qs/RotateString_796.py
@@ -13,3 +13,10 @@ class Solution:
             i += 1
 
         return False
+
+## ======================================================================================================
+# optimized solution
+		
+class Solution:
+    def rotateString(self, A: str, B: str) -> bool:
+        return len(A) == len(B) and B in A + A		
