655384042c4cede49d0513df42b11bfa0022f40e
aryastark11 <36256296+aryastark11@users.noreply.github.com>
2021-04-06 18:55:36 +0530

    Update Non-decreasingArray_665.py

--- Medium_Qs/Non-decreasingArray_665.py
+++ Medium_Qs/Non-decreasingArray_665.py
@@ -2,15 +2,16 @@ class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return True
-        pivot = []
+        pivot = []  # list to hold indices of elements that are out of order
         for i in range(1, len(nums)):
-            if nums[i] < nums[i-1]:
+            if nums[i] < nums[i-1]:  # in ascending order 2,4
                 try:
+                    # order is same -> 2,4,6
                     if nums[i+1] > nums[i-1]:
                         pivot.append(i)
-                    else:
+                    else:  # order is messed up -> 2,4,1 or 2,4,2
                         pivot.append(i-1)
-                except:
+                except:  # handle last element --> out of index error in list
                     pivot.append(i)
         print(pivot)
         if len(pivot) > 1:
@@ -19,6 +20,7 @@ class Solution:
             if nums == sorted(nums):
                 return True
             return False
+        # when pivot has only one element
         try:
             nums[pivot[0]] = nums[pivot[0]+1]
         except:
@@ -26,6 +28,7 @@ class Solution:
         if nums ==  sorted(nums):
             return True
         return False
+
 		
 		
 #### optimised solution
